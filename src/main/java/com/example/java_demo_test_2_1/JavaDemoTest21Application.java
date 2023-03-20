4d28854ef9beac9023116137ab354a88517d5c6a
Chihli0802 <annannwhat@gmail.com>
2023-03-20 11:45:11 +0800

    the first commit_java_demo_test_2_1

---
+++ src/main/java/com/example/java_demo_test_2_1/JavaDemoTest21Application.java
@@ -0,0 +1,13 @@
+package com.example.java_demo_test_2_1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaDemoTest21Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaDemoTest21Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/java_demo_test_2_1/JavaDemoTest21ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.java_demo_test_2_1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaDemoTest21ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
